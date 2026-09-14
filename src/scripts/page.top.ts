ab9156b8b7d5b1f067efde263d4400788fa4261c
“Ryu <yudougonglong727@gmail.com>
2026-09-14 17:58:34 +0900

    “”

---
+++ src/scripts/page.top.ts
@@ -0,0 +1,52 @@
+import { clamp } from "../utils/calc";
+import { RowScroll } from "../utils/rowScroll";
+
+function gallery_init() {
+  const wrapper = document.querySelector(".gallerySlider");
+  const nav = wrapper?.querySelector(".gallerySlider_nav_scroll")!;
+  const navItems = nav.querySelectorAll(".gallerySlider_nav_item");
+  const windowItems = wrapper?.querySelectorAll(".gallerySlider_window_item");
+
+  if (!nav || navItems.length <= 2) return;
+
+  let current = 0;
+  const total = navItems.length - 1;
+  let perGap = 0;
+  let perH = 0;
+
+  function onResize() {
+    const item1Rect = navItems[0].getBoundingClientRect();
+    const item2Rect = navItems[1].getBoundingClientRect();
+    perGap = item2Rect.top - item1Rect.bottom;
+    perH = item1Rect.height + perGap;
+
+    console.log(perH, perGap);
+  }
+  window.addEventListener("resize", onResize);
+  onResize();
+
+  function change() {
+    navItems.forEach((item, index) => {
+      item.classList.toggle("--current", index === current);
+    });
+    windowItems.forEach((item, index) => {
+      item.classList.toggle("js--hidden", index !== current);
+    });
+    console.log("j");
+  }
+
+  function onScroll() {
+    const scY = nav.scrollTop;
+    const next = Math.min(total, Math.max(0, Math.round(scY / perH)));
+
+    if (next !== current) {
+      current = next;
+      change();
+    }
+  }
+  new RowScroll({ wrapper: nav, onScroll: onScroll });
+}
+
+export function pageTop_init() {
+  gallery_init();
+}
--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -6,8 +6,8 @@ export function normalize(n: number, a: number, b: number): number {
   return (n - a) / (b - a);
 }
 
-export function clamp(n: number, isRound?: boolean): number {
-  return Math.max(0, Math.min(1, isRound ? round(n) : n));
+export function clamp(n: number, min?: number, max?: number): number {
+  return Math.max((min = 0), Math.min((max = 1), n));
 }
 
 export function round(n: number): number {
