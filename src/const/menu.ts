602dfdfb1493c0b22c2c3e0f06ed5c66582fe67c
“Ryu <yudougonglong727@gmail.com>
2026-09-14 14:27:47 +0900

    “”

---
+++ src/const/menu.ts
@@ -0,0 +1,54 @@
+export const toConcept = {
+  title: "Concept",
+  href: "#concept",
+};
+export const toFacility = {
+  title: "Facility",
+  href: "#facility",
+};
+export const toAccess = {
+  title: "Access",
+  href: "#access",
+};
+export const toFaq = {
+  title: "Faq",
+  href: "#faq",
+};
+
+export const toTeam = {
+  title: "利用規約",
+  href: "/terms/",
+};
+export const toPrivacy = {
+  title: "プライバシーポリシー",
+  href: "/privacy/",
+};
+export const toLegal = {
+  title: "特定商取引",
+  href: "/legal/",
+};
+export const toAccommodation = {
+  title: "宿泊約款",
+  href: "/accommodation/",
+};
+
+export const toYoutube = {
+  title: "Youtube",
+  href: "/",
+};
+export const toInstagram = {
+  title: "Instagram",
+  href: "/",
+};
+export const toIds = {
+  title: "IDS",
+  href: "/",
+};
+export const toOnlineStore = {
+  title: "Online Store",
+  href: "/",
+};
+
+export const menuData_main = [toConcept, toFacility, toAccess, toFaq];
+export const menuData_sub = [toTeam, toPrivacy, toLegal, toAccommodation];
+export const menuData_Follow = [toYoutube, toInstagram, toIds, toOnlineStore];
